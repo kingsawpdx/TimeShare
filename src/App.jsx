35d51b4f2f00cddedc98777fb822d567f19b3b3d
Raghad Al Musawi <raghad3@pdx.edu>
2025-02-12 09:56:07 -0800

    comments removed from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,6 @@ function App() {
       </Routes>
     </Router>
   );
-
-  // const [ flaskHello, setFlaskHello ] = useState("Error!");
-
-  // useEffect( () => {
-  //   fetch("/api/hello")
-  //     .then( res => res.json() )
-  //     .then( jdata => { setFlaskHello( jdata.hello ); });
-  //   }, []);
-
-  // return (
-  //   <div>
-  //     <h1>React App Boilerplate</h1>
-  //     <p> Hello! {flaskHello} </p>
-  //   </div>
-  // );
 }
 
 export default App;
